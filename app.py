ae79b19bb7c6db369454fd5a9aeb50fd15b96b71
uinstinct <uinstinct@outlook.com>
2020-08-31 01:59:30 +0530

    initialized database

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filer.db'
+db = SQLAlchemy(app)
+
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False) # this field cannot be blank
+    data_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Movie %r>' % self.id
 
 @app.route('/')
 def index():
-    return 'initialized the app'
+    movies = Movies.query.order_by(Movies.data_added).all()
+    return 'working till now'
 
 if __name__ == '__main__':
     app.run(debug=True)
