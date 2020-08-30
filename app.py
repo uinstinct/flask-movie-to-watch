b8cb889e76d867f1da7a41acbe18d5e4797ee52f
uinstinct <uinstinct@outlook.com>
2020-08-31 02:32:11 +0530

    delete route working

--- app.py
+++ app.py
@@ -6,18 +6,40 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filer.db'
 db = SQLAlchemy(app)
 
+# database being created here
 class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False) # this field cannot be blank
-    data_added = db.Column(db.DateTime, default=datetime.utcnow)
-
+    name = db.Column(db.String(200), nullable=False) # this means this field cannot be blank
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
     def __repr__(self):
         return '<Movie %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    movies = Movies.query.order_by(Movies.data_added).all()
-    return 'working till now'
+    if request.method == 'POST':
+        movie_content = request.form['content']
+        new_movie = Movies(name=movie_content)
+        try:
+            db.session.add(new_movie)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Problem adding that movie into the database'
+    else:
+        movies = Movies.query.order_by(Movies.date_added).all()
+        return render_template('index.html',movies=movies)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    deletingMovie = Movies.query.get_or_404(id)
+
+    try:
+        db.session.delete(deletingMovie)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'That movie had a problem while deleting'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
